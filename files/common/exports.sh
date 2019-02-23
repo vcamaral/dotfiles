4d1e17626c6478ea1242e957530858393246d3d9
Vinicius Amaral <vcamaral@gmail.com>
2019-02-23 18:11:15 -0300

    Adds dotnet in PATH.

--- files/common/exports.sh
+++ files/common/exports.sh
@@ -1,6 +1,6 @@
 export EDITOR=${EDITOR:-vim}
 export VISUAL=${VISUAL:-vim}
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/sbin:$HOME/local/bin"
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/sbin:/usr/local/share/dotnet:$HOME/local/bin"
 export CDPATH=.:$HOME/Code
 export ZSH="$HOME/.oh-my-zsh"
 export ZSH_THEME="robbyrussell"
