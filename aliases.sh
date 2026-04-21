8f84a8581f92899b53d2b720e05f59a60e11d9fe
Vinicius Amaral <vcamaral@gmail.com>
2026-04-21 19:51:45 -0300

    add brew update alias

--- aliases.sh
+++ aliases.sh
@@ -51,3 +51,8 @@ alias paste="pbpaste"
 # ──────────────────────────────────────────────
 alias localip="ipconfig getifaddr en0"
 alias externalip="curl -s https://ipinfo.io/ip && echo"
+
+# ──────────────────────────────────────────────
+# Homebrew
+# ──────────────────────────────────────────────
+alias brewup="brew upgrade \$(brew outdated)"
